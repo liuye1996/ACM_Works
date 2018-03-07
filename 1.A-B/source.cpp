5f1e2e49915d48200060284f1bbab041494b209c
liuye1996 <31651531+liuye1996@users.noreply.github.com>
2018-03-08 00:11:41 +0800

    Create source.cpp

---
+++ 1.A-B/source.cpp
@@ -0,0 +1,27 @@
+/*
+2018/3/6
+A+B Problem
+时间限制：3000 ms  |  内存限制：65535 KB
+难度：0
+描述
+此题为练手用题，请大家计算一下a+b的值.
+输入
+输入两个数，a,b
+输出
+输出a+b的值
+样例输入
+2 3
+样例输出
+5
+*/
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	system("pause");
+}
